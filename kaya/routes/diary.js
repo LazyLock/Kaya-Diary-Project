49b35222415182c89317fdc63951e749b2bbedb4
LazyLock <a01058121097@gmail.com>
2022-06-21 23:48:30 +0900

    다이어리 확인하는 라우터 생성
    홈화면 버튼 생성, 넌적스 레이아웃을 이용해 html 구조화

--- kaya/routes/diary.js
+++ kaya/routes/diary.js
@@ -7,13 +7,26 @@ router.post("/", async (req, res, next) => {
   const { date, catego, friend, title, comment } = req.body;
   try {
     await Diary.create({
-      date,
-      catego,
-      friend,
-      title,
-      comment,
+      date: date,
+      category: catego,
+      friends: friend,
+      title: title,
+      comment: comment,
     });
     res.redirect("/");
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.get("/:id/detail", async (req, res, next) => {
+  try {
+    const diary_selected = await Diary.findOne({
+      where: { id: req.params.id },
+    });
+    const diarys = await Diary.findAll({});
+    res.render("diary_detail", { diary_selected, diarys });
   } catch (err) {
     console.error(err);
     next(err);
